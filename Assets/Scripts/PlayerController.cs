437fa3d5065a0212079979824b52771d783d8633
Bryan França <43351790+bryan-fm@users.noreply.github.com>
2022-10-22 12:03:19 -0300

    ativando animaçoes ao andar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
     public Rigidbody2D rb;
     public float moveSpeed;
 
+    public Animator myAnim;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -15,5 +17,16 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        myAnim.SetFloat("moveX", rb.velocity.x);
+        myAnim.SetFloat("moveY", rb.velocity.y);
+
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || 
+            Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1) {
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+        
     }
 }
