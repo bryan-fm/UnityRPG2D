d23dfcf68b26e3160d652e2b6e48e9e3bac55cd0
Bryan França <43351790+bryan-fm@users.noreply.github.com>
2022-10-23 21:35:15 -0300

    alterando cenas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,9 +9,18 @@ public class PlayerController : MonoBehaviour {
 
     public Animator myAnim;
 
+    public static PlayerController instance;
+
     // Start is called before the first frame update
     void Start() {
-        
+        if(instance == null) {
+
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
