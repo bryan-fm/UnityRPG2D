f90dbda3a623edbe7c4d8ebf705dff383eb0dc74
Bryan França <43351790+bryan-fm@users.noreply.github.com>
2022-11-02 19:54:21 -0300

    funcao para retornar detalhes do item

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour
 
     public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
 
+    public string[] itemsHeld;
+    public int[] numberOfItems;
+    public Item[] referenceItems;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +29,19 @@ public class GameManager : MonoBehaviour
         } else {
             PlayerController.instance.canMove = true;
         }
+    }
+
+    public Item GetItemDetails(string itemToGrab)
+    {
+        for (int i = 0; i < referenceItems.Length; i++) {
+
+            if (referenceItems[i].itemName == itemToGrab) {
+
+                return referenceItems[i];
+            }
+            
+        }
+
+        return null;
     }
 }
