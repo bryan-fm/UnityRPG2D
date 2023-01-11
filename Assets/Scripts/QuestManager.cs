4707192b3f247ee11f1f7934fa5ca42711f99ba0
Bryan França <43351790+bryan-fm@users.noreply.github.com>
2023-01-10 21:16:07 -0300

    changes

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -64,6 +64,7 @@ public class QuestManager : MonoBehaviour
         return false;
     }
 
+
     public void MarkQuestComplete(string questToMark)
     {
         questMarkersComplete[GetQuestNumber(questToMark)] = true;
